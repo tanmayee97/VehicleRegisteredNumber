bd295c56be27fd93a9ffaa95283ae636a79a4083
tanmayee97 <42970999+tanmayee97@users.noreply.github.com>
2018-09-17 16:57:39 +0530

    VehicleRegisteredNumber

---
+++ BikeRegistrationNumber.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.*;
+public class BikeRegistrationNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter your Car Registered number");
+		String a=sc.next();
+		carnumber(a);
+	}
+public static boolean carnumber(String s)
+{
+	if(s.length()==10)
+	{
+		if(s.charAt(0)>=65 && s.charAt(0)<=90 && s.charAt(1)>=65 && s.charAt(1)<=90 &&
+				s.charAt(2)>=48 && s.charAt(2)<=57 && s.charAt(3)>=48 && s.charAt(3)<=57 &&
+				s.charAt(4)>=65 && s.charAt(4)<=90 && s.charAt(5)>=65 && s.charAt(5)<=90 && 
+				s.charAt(6)>=48 && s.charAt(6)<=57 && s.charAt(7)>=48 && s.charAt(7)<=57 &&
+				s.charAt(8)>=48 && s.charAt(8)<=57 && s.charAt(9)>=48 && s.charAt(9)<=57) 
+		          {
+					System.out.println("Valid Bike Registered Number");
+				  }
+					else
+					{
+						System.out.println("Enter Correct Bike Registered number");
+					}
+					}
+	else
+	{
+	System.out.println("Enter Valid Bike Registered number");
+}
+
+
+return true;
+}
+}
